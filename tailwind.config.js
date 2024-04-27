c93124272a7fe2f8bec238d88a81b1827e862f81
Daniel Rodriguez <daniel.rodriguez003@xpo.com>
2024-04-27 12:28:23 -0500

    Added landing components

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#0A192F",
+        primary: "#64FFDB",
+        secondary: "#CCD6F6",
+      },
+    },
+  },
+  plugins: [],
+};
