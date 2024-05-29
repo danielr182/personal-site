939f1af5eb8c783e059256e0ab3c9425fd9a409c
Daniel Rodriguez M <daniel.rodriguez182@gmail.com>
2024-05-29 18:09:22 -0500

    Updated contact and footer components

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  readonly date = new Date();
+  readonly footer = `© ${this.date.getFullYear()} Made by Daniel Rodríguez`;
 }
--- src/app/pages/home/contact/contact.component.ts
+++ src/app/pages/home/contact/contact.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Section } from '../../../core/enums/section.enum';
+import { ScreenUtils } from '../../../core/utils/screen-utils';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-
+  gotoHome(): void {
+    ScreenUtils.goToSpecificElementById(Section.home);
+  }
 }
