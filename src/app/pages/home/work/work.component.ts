1eb59cfaab69d569dca984c841bd79d0c5ea0d42
Daniel Rodriguez M <daniel.rodriguez182@gmail.com>
2024-06-11 15:11:06 -0500

    Added new work item

--- src/app/pages/home/work/work.component.ts
+++ src/app/pages/home/work/work.component.ts
@@ -11,6 +11,7 @@ import { MyWork } from '../../../core/interfaces/my-work';
 export class WorkComponent {
   private readonly difrutiUrl = 'https://difruti.com/';
   private readonly expensegerUrl = 'https://income-expense-app-danielr182s-projects.vercel.app/';
+  private readonly adminproUrl = 'https://admin-angular-danielr182s-projects.vercel.app/';
   private readonly spotiUrl = 'https://spoti-danielr182s-projects.vercel.app/';
   readonly myWorks: MyWork[] = [
     {
@@ -26,6 +27,30 @@ export class WorkComponent {
       name: 'Difruti',
       technologies: ['Angular', 'Bootstrap', 'WhatsApp API', 'Mailchimp'],
       url: this.difrutiUrl,
+    },
+    {
+      description: `This website allows users to manage medics and hospitals (CRUD). 
+        It has a login function using credentials registered in the database or using Google identity. 
+        The data is stored in a mongoDB through a node JS application that exposes a REST API. 
+        For API authentication and information exchange the app uses JWT. An AdminPro template is used from wrappixel.`,
+      image: 'adminpro.png',
+      links: [
+        {
+          icon: 'github.svg',
+          url: 'https://github.com/danielr182/adminAngular/',
+        },
+        {
+          icon: 'github.svg',
+          url: 'https://github.com/danielr182/backend-server-adv/',
+        },
+        {
+          icon: 'external-link.svg',
+          url: this.adminproUrl,
+        },
+      ],
+      name: 'AdminPro',
+      technologies: ['Angular', 'Node JS', 'MongoDB', 'Google Identity', 'JWT', 'Bootstrap'],
+      url: this.adminproUrl,
     },
     {
       description: `This website allows users to track income and expenses. It has a login feature through Firebase. The data is stored in cloud firestore. 
